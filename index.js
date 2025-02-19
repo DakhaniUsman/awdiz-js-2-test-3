f8bee2f552d3dfa15cb4b2c92d18a1bb99ac677c
Dakhani Usman <115381340+DakhaniUsman@users.noreply.github.com>
2025-02-19 09:42:04 +0530

    JS 2 TEST 3

--- index.js
+++ index.js
@@ -31,37 +31,35 @@ var array = [4, 1, 6, 2, 8];
 var array = [10, 20, 30, 5, 7];
 
 const Smallest = (array) => {
-
   let smallest = Infinity;
   let secondSmallest = Infinity;
 
   for (let i = 0; i < array.length; i++) {
-
     console.log(array[i]);
 
-    if (array[i] < smallest) {       // 4 < infinity  1 < 4 true
+    if (array[i] < smallest) {
+      // 4 < infinity  1 < 4 true
       secondSmallest = smallest; //  secondSmallest = inifinity
-      smallest = array[i];       //  smallest = 4
+      smallest = array[i]; //  smallest = 4
 
+      console.log("inside if");
+      console.log(
+        ` array[${i}] : ${array[i]} -- smallest : ${smallest} -- secondSmallest : ${secondSmallest}  `
+      );
+    } else if (array[i] < secondSmallest) {
+      // 1 < infinity
 
-      console.log("inside if")
-      console.log(` array[${i}] : ${array[i]} -- smallest : ${smallest} -- secondSmallest : ${secondSmallest}  `)
-    } else if (array[i] < secondSmallest) { // 1 < infinity
+      console.log("inside else if");
 
-      console.log("inside else if")
-
-      secondSmallest = array[i];    // secondSmallest = 1
-      console.log(`secondSmallest : ${secondSmallest}`)
-
+      secondSmallest = array[i]; // secondSmallest = 1
+      console.log(`secondSmallest : ${secondSmallest}`);
     }
   }
 
   return secondSmallest;
-
 };
 
 // console.log(Smallest(array));
-
 
 // var array = [10,20,30,5,7];
 
@@ -70,7 +68,6 @@ const Smallest = (array) => {
 //   let smallest = Infinity;
 //   let secondSmallest = Infinity;
 
-
 //   for(let i = 0 ; i < array.length ; i ++){
 
 //     if (array[i] < smallest){
@@ -78,8 +75,6 @@ const Smallest = (array) => {
 //       secondSmallest = smallest;
 
 //       smallest = array[i];
-
-
 
 //     }
 
@@ -95,73 +90,124 @@ const Smallest = (array) => {
 
 // console.log(SecondSmallest(array))
 
+// find the second smallest number from an array
 
+var array = [10, 20, 30, 5, 6, 7];
+
+function SecondSmallest(array) {
+  let smallest = Infinity;
+  let secondSmallest = Infinity;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < smallest) {
+      secondSmallest = smallest;
+      smallest = array[i];
+    } else if (array[i] > smallest && array[i] < secondSmallest) {
+      secondSmallest = array[i];
+    }
+  }
+
+  return secondSmallest;
+}
+
+// console.log(SecondSmallest(array));
+
+// find second largest
+
+var array = [10, 30, 40, 99, 49];
+
+const SecondLargest = (array) => {
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > largest) {
+      secondLargest = largest;
+
+      largest = array[i];
+    } else if (array[i] < largest && array[i] > secondLargest) {
+      secondLargest = array[i];
+    }
+  }
+
+  return `second largest : ${secondLargest}`;
+};
+
+// console.log(SecondLargest(array));
 
 // Q.3
 
-var word1 = "hello world";
+var word1 = "JavaScript is fun";
 
-const ReverseWords = (word) => {
-  console.log(word);
-let count = 0
-  for (let i = 0 ; i < word.length ; i++){
+const ReverseWords = (string) => {
+  var word = "";
+  var result = "";
 
-    if(word[i] == " "){
-      break;
-
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] !== " ") {
+      word = string[i] + word;
+      // console.log(`word : ${word}`);
+    } else {
+      result += word + " ";
+      word = "";
+      // console.log(`result : ${result}`);
     }
-    console.log(count)
-    console.log(word[i]);
-
-    var array1 =
-    count++;
   }
 
-  for(let j = count ; j < word.length ; j ++){
-    console.log(word[j])
-  }
-
-
-
+  result += word;
+  return result;
 };
 
 console.log(ReverseWords(word1));
 
-
 // Q. 4 find missing numebr in array
 
 var array = [1, 2, 4, 5];
-var array = [1, 3, 4, 5, 6];
+var n = 5;
+// var array = [1, 3, 4, 5, 6];
+// var n = 6;
 
-const missingNumber = (array) => {
-  for (let i = 1; i < array.length; i++) {
-    if (array[i - 1] !== i) {
-      return i;
-    }
+const MissingNumber = (array, n) => {
+  let expectedSum = (n * (n + 1)) / 2;
+
+  let actualSum;
+
+  let missingNumber;
+
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
   }
+
+  // actualSum = array.reduce((acc,cv)=> acc + cv, 0)
+
+  actualSum = sum;
+
+  missingNumber = expectedSum - actualSum;
+
+  return ` missingNumber : ${missingNumber}`;
 };
 
-// console.log(missingNumber(array));
+// console.log(MissingNumber(array, n));
 
 var string = "aaabbcddd";
 
 const StringCompression = (string) => {
-  let output = {};
+  let result = "";
+
+  let count = 1;
 
   for (let i = 0; i < string.length; i++) {
-    let count = 0;
-
-    for (let j = 0; j < string.length; j++) {
-      if (string[i] == string[j]) {
-        count++;
-        output[string[i]] = count;
-        console.log(output);
-      }
+    if (string[i] === string[i + 1]) {
+      count++;
+    } else {
+      result += string[i] + count;
+      count = 1;
     }
-    // for ()
   }
 
-  return output;
+  return result;
 };
 
-// console.log(StringCompression(string));
+console.log(StringCompression(string));
